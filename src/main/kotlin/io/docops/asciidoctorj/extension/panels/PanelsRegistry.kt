25362354f7daa4915278d91376327b3268b78e3d
Steve Roach <sroachst@gmail.com>
2022-03-01 00:02:33 -0500

    tocbot

--- src/main/kotlin/io/docops/asciidoctorj/extension/panels/PanelsRegistry.kt
+++ src/main/kotlin/io/docops/asciidoctorj/extension/panels/PanelsRegistry.kt
@@ -26,6 +26,8 @@ class PanelsRegistry : ExtensionRegistry {
         //registry.docinfoProcessor(FeedbackDocinfoProcessor::class.java)
         // registry.docinfoProcessor(FeedbackDivDocInfoProcessor::class.java)
         registry.blockMacro(ColorMapBlockMacroProcessor::class.java)
+        registry.docinfoProcessor(TocBotHeaderDocInfoProcessor::class.java)
+        registry.docinfoProcessor(TocBotFooterDocInfoProcessor::class.java)
     }
 
 }
